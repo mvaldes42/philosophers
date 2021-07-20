b6de8396c88ad5bdbceadddb098964434ae42e84
mvaldes <mvaldes@student.42.fr>
2021-07-20 11:12:02 +0200

    norm

--- srcs/init_inputs.c
+++ srcs/init_inputs.c
@@ -6,7 +6,7 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 11:52:26 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/07/20 10:47:27 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/07/20 11:10:54 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,31 @@
 
 static void	exit_failure(t_innkeper *inn)
 {
-	if (inn->p)
-		free(inn->p);
-	exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
 
-static void	parse_inputs(int argc, char **argv, t_innkeper *inn, t_inputs *in)
+static void	args_are_valid(int argc, char **argv, t_innkeper *inn, t_inputs *in)
 {
-	gettimeofday(&inn->in_ptr.start_time, NULL);
+	if (argc < 4)
+		exit_failure(inn);
 	if (strchr(argv[1], '.') || strchr(argv[2], '.') || strchr(argv[3], '.') \
 	|| strchr(argv[4], '.') || \
 	strchr(argv[1], ',') || strchr(argv[2], ',') || strchr(argv[3], ',') \
 	|| strchr(argv[4], ','))
 		exit_failure(inn);
+	if (argc == 6 && (strchr(argv[5], ',') || strchr(argv[5], '.')))
+		exit_failure(inn);
+}
+
+static void	parse_inputs(int argc, char **argv, t_innkeper *inn, t_inputs *in)
+{
+	gettimeofday(&inn->in_ptr.start_time, NULL);
 	in->nb_p = atoi(argv[1]);
 	in->time_die = atoi(argv[2]);
 	in->time_eat = atoi(argv[3]);
 	in->time_sleep = atoi(argv[4]);
 	if (argc == 6)
-	{
-		if (strchr(argv[5], ',') || strchr(argv[5], '.'))
-			exit_failure(inn);
 		in->plts_p_philo = atoi(argv[5]);
-	}
 	else
 		in->plts_p_philo = __LONG_LONG_MAX__;
 	if (in->nb_p % 2 == 1)
@@ -55,11 +57,10 @@ void	init_inputs(int argc, char **argv, t_innkeper *inn)
 	t_inputs	*inputs;
 	int			i;
 
-	if (argc < 4)
-		exit_failure(inn);
 	inputs = &inn->in_ptr;
 	memset(inputs, 0, sizeof(*inputs));
 	memset(&inn->s_in, 0, sizeof(inn->s_in));
+	args_are_valid(argc, argv, inn, inputs);
 	parse_inputs(argc, argv, inn, inputs);
 	inn->p = malloc(inn->in_ptr.nb_p * sizeof(t_philo));
 	i = 0;
